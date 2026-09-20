58ad853d01d7d44a4a01277c01d39c7e31850bac
VARUN-MERCURY <144169390+VARUN-MERCURY@users.noreply.github.com>
2026-09-20 13:53:37 +0530

    Add solution 2 for Rearrange Array Elements by Sign (Medium)

---
+++ rearrange-array-elements-by-sign/solution2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+
+        int p=0;
+        int n=1;
+          
+        vector<int> ans(nums.size(), 0);
+
+        for(int i=0;    i<nums.size();   i++)
+        {
+
+            if(nums[i]>0)
+            {
+                ans[p]= nums[i];
+                p=p+2;
+            }
+            else
+            {
+                ans[n]=nums[i];
+                n=n+2;
+            }
+        }    
+        return ans;
+        
+    }
+};
